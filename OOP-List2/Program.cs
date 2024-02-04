aadbfd66893e43f91a9b4921374ff2f4480c729d
janthony25 <junanthony.business@gmail.com>
2024-02-04 13:35:27 +1300

    Add project files.

---
+++ OOP-List2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // List - Practice
+
+            List<string> foodChain = new List<string>();
+
+            foodChain.Add("Jollibee");
+            foodChain.Add("McDonalds");
+            foodChain.Add("KFC");
+            foodChain.Remove("KFC");
+            Console.WriteLine(foodChain.Contains("Jollibee"));
+
+            foreach (string store in foodChain)
+            {
+                Console.WriteLine(store);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
